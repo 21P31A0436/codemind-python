a6c5a083b257ebc164445638b6f5218f859c2093
21P31A0436 <99795947+21P31A0436@users.noreply.github.com>
2022-10-21 22:45:07 +0530

    n=int(input()) sum=0 c=0 arr=list(map(int,input().split())) for i in range(len(arr)):     sum=sum+arr[i] avg=sum/n for j in range(n):     if arr[j]<=avg:         c+=1 print(c)

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,11 @@
+n=int(input())
+sum=0
+c=0
+arr=list(map(int,input().split()))
+for i in range(len(arr)):
+    sum=sum+arr[i]
+avg=sum/n
+for j in range(n):
+    if arr[j]<=avg:
+        c+=1
+print(c)
